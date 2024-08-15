5ee375b60ae345db46cdb0c274f29a28db47907e
0xRnd <renanndonato@gmail.com>
2024-08-15 10:51:07 -0300

    using chainlink and library to check eth value in usd

---
+++ Contracts/FundMe/FundMe.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {PriceConverter} from "./PriceConverter.sol";
+
+contract FundMe{
+    using PriceConverter for uint256;
+    uint256 public minimumUsd = 5e18;
+
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= minimumUsd, "Not enough USD"); 
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }    
+}
